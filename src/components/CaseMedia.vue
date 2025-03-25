b675bbd333f5d9f9488a8dfc29b27c65de434d48
itsalexcm <hello.alexcerezo@gmail.com>
2025-03-25 23:06:45 +0100

    Remove directives

--- src/components/CaseMedia.vue
+++ src/components/CaseMedia.vue
@@ -2,7 +2,7 @@
   <div v-if="layout === 'column'" class="case-media-column">
     <div v-if="layout === 'column' || layout === 'row'" class="case-media-row">
       <div class="case-media">
-        <div v-for="(image, index) in images" :key="index" class="case-img" v-image-zoom>
+        <div v-for="(image, index) in images" :key="index" class="case-img">
           <img :src="image.src" :alt="image.alt" class="case-img-zoom">
           <button class="case-img-open"></button>
         </div>
@@ -13,7 +13,7 @@
 
   <div v-else-if="layout === 'row'" class="case-media-row">
     <div class="case-media">
-      <div v-for="(image, index) in images" :key="index" class="case-img" v-image-zoom>
+      <div v-for="(image, index) in images" :key="index" class="case-img">
         <img :src="image.src" :alt="image.alt" class="case-img-zoom">
         <button class="case-img-open"></button>
       </div>
@@ -22,7 +22,7 @@
   </div>
 
   <div v-else class="case-media">
-    <div v-for="(image, index) in images" :key="index" class="case-img" v-image-zoom>
+    <div v-for="(image, index) in images" :key="index" class="case-img">
       <img :src="image.src" :alt="image.alt" class="case-img-zoom">
       <button class="case-img-open"></button>
     </div>
@@ -31,13 +31,44 @@
 </template>
 
 <script setup>
-defineProps({
-  images: Array,   
-  caption: String,
-  layout: {
-    type: String,
-    default: '',
-    validator: (value) => ['', 'row', 'column'].includes(value)
-  }
-});
+  defineProps({
+    images: Array,   
+    caption: String,
+    layout: {
+      type: String,
+      default: '',
+      validator: (value) => ['', 'row', 'column'].includes(value)
+    }
+  });
 </script>
+
+<style scoped>
+  .case-img-fullscreen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0,0,0,0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .case-img-close {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    background: none;
+    border: none;
+    font-size: 32px;
+    color: white;
+    cursor: pointer;
+    z-index: 10000;
+  }
+
+  .case-img.hidden {
+    visibility: hidden;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -5,16 +5,8 @@ import router from './router';
 // Importar estilos globales
 import '@/assets/styles/main.scss';
 
-// Importar directivas
-import vFadeIn from '@/directives/vFadeIn.js';
-import vImageZoom from '@/directives/vImageZoom.js';
-
 // Crear la app
 const app = createApp(App);
-
-// Registrar directivas
-app.directive('fade-in', vFadeIn);
-app.directive('image-zoom', vImageZoom);
 
 app.use(router);
 app.mount('#app');
