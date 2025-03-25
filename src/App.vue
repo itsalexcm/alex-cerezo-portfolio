cb6d95e839d335b4ce825c62cc90a2afd86e12ae
itsalexcm <hello.alexcerezo@gmail.com>
2025-03-25 23:05:45 +0100

    Remove some comments

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/profile">Profile</router-link>
-  </nav> -->
   <ThemeToggle />
   <KonamiEffect />
   <router-view/>
 </template>
 
 <script setup>
-import ThemeToggle from '@/components/ThemeToggle.vue';
-import KonamiEffect from '@/components/KonamiEffect.vue';
-import { useTheme } from '@/composables/useTheme.js';
-import { setupFadeInOnLoad } from '@/composables/useFadeIn.js';
+  import ThemeToggle from '@/components/ThemeToggle.vue';
+  import KonamiEffect from '@/components/KonamiEffect.vue';
+  import { useTheme } from '@/composables/useTheme.js';
+  import { setupFadeInOnLoad } from '@/composables/useFadeIn.js';
 
-useTheme();
-setupFadeInOnLoad();
+  useTheme();
+  setupFadeInOnLoad();
 </script>
