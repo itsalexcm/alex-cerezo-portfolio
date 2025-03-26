77b88c677be23def588db9c9ac518eb1188215e3
itsalexcm <hello.alexcerezo@gmail.com>
2025-03-26 19:04:14 +0100

    Componetization

---
+++ src/components/HeaderBrand.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="alex">
+    <h1>Alex Cerezo 👋</h1>
+    <router-link to="/">
+      <LogoComponent />
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import LogoComponent from './LogoComponent.vue';
+</script>
---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <menu>
+    <li>
+      <router-link to="/profile" class="text large menu-link">Profile</router-link>
+    </li>
+    <li>
+      <ThemeToggle />
+    </li>
+  </menu>
+</template>
+
+<script setup>
+import ThemeToggle from './ThemeToggle.vue';
+</script>
