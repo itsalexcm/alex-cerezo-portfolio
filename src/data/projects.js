c2b87461f139f6d98929e5a408550640ebf40195
itsalexcm <hello.alexcerezo@gmail.com>
2025-03-27 20:46:49 +0100

    Update img

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
-import designSystem from './projects/design-system.js';
 import smarterCalendar from './projects/smarter-calendar.js';
+import designSystem from './projects/design-system.js';
 import redesignData from './projects/redesign-data.js';
 import scalingDesign from './projects/scaling-design.js';
 import slackCaseStudy from './projects/slack-case-study.js';
 
-export const projects = [designSystem, smarterCalendar, redesignData, scalingDesign, slackCaseStudy];
+export const projects = [smarterCalendar, designSystem, redesignData, scalingDesign, slackCaseStudy];
