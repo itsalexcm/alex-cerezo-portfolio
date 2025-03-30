90b810a4bc5759717c21f2c7e08076117f0113af
itsalexcm <hello.alexcerezo@gmail.com>
2025-03-31 00:53:25 +0200

    Remove comments

--- src/composables/useFadeIn.js
+++ src/composables/useFadeIn.js
@@ -1,16 +1,14 @@
 export function runFadeIn() {
   const elements = document.querySelectorAll('.fade-in');
 
-  // Garantiza clases en body
   document.body.classList.add('enable-transitions');
   if (!document.body.classList.contains('theme-ready')) {
     document.body.classList.add('theme-ready');
   }
 
-  // Reinicia y aplica la animación
   elements.forEach(el => el.classList.remove('visible'));
 
   setTimeout(() => {
     elements.forEach(el => el.classList.add('visible'));
-  }, 100); // un poco más reactivo que 200ms
+  }, 100);
 }
