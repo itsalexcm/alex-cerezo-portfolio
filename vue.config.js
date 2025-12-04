010011e70a27526c594d09ce0cfe479b8022e04c
Alex Cerezo <hello.alexcerezo@gmail.com>
2025-12-04 13:12:15 +0100

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,15 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
-        __VUE_OPTIONS_API__: true,  // Asegura que Vue Options API funcione
-        __VUE_PROD_DEVTOOLS__: false, // Desactiva devtools en producción
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false // 🔥 Solución para el error
-      })
-    ]
-  }
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+      }),
+    ],
+  },
 });
